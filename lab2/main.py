e6d2eb7ab05d32867e78a97ec65a31069483138f
marisasha <marisasha228@bk.ru>
2025-11-16 18:23:39 +0300

    update regular and main

--- lab2/main.py
+++ lab2/main.py
@@ -4,9 +4,43 @@ from regular import PhoneNumberExtractor
 if __name__ == "__main__":
 
     extractor = PhoneNumberExtractor()
+    
+    # =-=-Поиск телефонов по URL-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
     url = "https://moscow.shop.megafon.ru/connect/chnumber/fullnumber"
     
+    print(f"Поиск по странице: {url}")
+
     phones = extractor.get_phones_from_url(url)
+    if phones:
+        for i, phone in enumerate(phones, 1):
+            print(f"{i:2d}. {phone}")
+    else:
+        print("Номера не найдены")
+
+    # =-=-Поиск телефонов по тексту-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    print("\n\Поиск по тексту тексту:\n")
+
+    test_text = """
+    Контактные номера:
+    Основной: +7 (495) 123-45-67
+    Мобильный: 8 (916) 123-45-67
+    Еще: 495-123-45-67
+    """
     
-    print(phones)
+
+    test_phones = extractor.extract_phone_numbers(test_text)
+    for phone in test_phones:
+        is_valid = extractor.validate_phone_number(phone)
+        status = "VALID" if is_valid else "INVALID"
+        print(f"{phone} - {status}")
+
+    # =-=-Поиск телефонов по файлу-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+    
+    print("\nПоиск по файлу\n")
+
+    with open("phone.txt","r") as file:
+        file_phones = extractor.extract_phone_numbers(file.read())
+        for phone in file_phones:
+            print(f"{phone}")
