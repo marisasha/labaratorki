a41f7b016b03bb2b2874704a02cb2bec0b946e37
marisasha <marisasha228@bk.ru>
2025-12-19 19:13:32 +0300

    add lab4

---
+++ lab4/handlers/start.py
@@ -0,0 +1,14 @@
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram import html
+
+router = Router()  
+
+
+@router.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(
+        f"Привет, {html.bold(message.from_user.full_name)}!\n"
+        "Чтобы узнать погоду /weather "
+    )
